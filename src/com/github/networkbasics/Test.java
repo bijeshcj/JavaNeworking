ab6d3777cc072f8b3b0fd0841e281f53d6375bcb
bijeshcj <bijesh.jayaprakash@gmail.com>
2013-07-19 06:13:20 +0530

    initialized the project

---
+++ src/com/github/networkbasics/Test.java
@@ -0,0 +1,14 @@
+package com.github.networkbasics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bijesh C J
+ * Date: 19/7/13
+ * Time: 6:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String... str){
+        System.out.println("Github established...");
+    }
+}
