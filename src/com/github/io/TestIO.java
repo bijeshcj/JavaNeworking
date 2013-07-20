7750ef6a3926bbd33971dc51aedc91a7e49e34bf
NPCompete <npcompete@npcompete-desktop.(none)>
2013-07-20 11:34:26 +0530

    IO classes initilazed

---
+++ src/com/github/io/TestIO.java
@@ -0,0 +1,14 @@
+package com.github.io;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: npcompete
+ * Date: 20/7/13
+ * Time: 11:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestIO {
+    public static void main(String[] str){
+        System.out.println("IO classes initialzed....");
+    }
+}
