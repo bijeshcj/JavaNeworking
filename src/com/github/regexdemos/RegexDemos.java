e1ef25fd2fe21fab663395f5fb5c52b325faf793
bijeshcj <bijesh.jayaprakash@gmail.com>
2013-07-20 21:42:40 +0530

    regex demos: scans newline

--- src/com/github/regexdemos/RegexDemos.java
+++ src/com/github/regexdemos/RegexDemos.java
@@ -15,12 +15,23 @@ public class RegexDemos {
         String x =  "{\"time\" : \"2012-09-24T03:08:50\", \"message\" : \"Call() started\"}";
         String pat = "\\{(.*?)\\}";
 //        getNumberOfBracesFromGivenString(getString(),"\\{(.*?)\\}");
-        getNumberOfBracesFromGivenString(x,pat);
+        getNumberOfBracesFromGivenString(getString(),pat);
+//        scanNewLine();
     }
     private String getString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("public class Test{\n").append("public Test(){\n hi there}\n}");
+        sb.append("public class Test{\n").append("public Test(){\n hi there\n}\n}");
         return sb.toString();
+    }
+    private void scanNewLine(){
+        String s = "hi there\nfind how many lines\n";
+        Pattern pat = Pattern.compile("\\n");
+        Matcher mat = pat.matcher(s);
+        int count =0;
+        while(mat.find()){
+            count++;
+        }
+        System.out.println("Number of lines are "+count);
     }
     private void getNumberOfBracesFromGivenString(String s,String pattern){
         System.out.println(s);
